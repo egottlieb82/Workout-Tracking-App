e3202084546b749c04f8fbcfc6e4c16dd71cc7d2
egottlieb82 <egottlieb82@gmail.com>
2024-08-05 19:31:41 -0500

    Initial Commit

---
+++ Workout Tracking App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout Tracking App
+//
+//  Created by Eli Gottlieb on 8/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout Tracking App/Workout_Tracking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_Tracking_AppApp.swift
+//  Workout Tracking App
+//
+//  Created by Eli Gottlieb on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Workout_Tracking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
